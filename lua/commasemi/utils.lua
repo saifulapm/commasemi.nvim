cbfa3554e554f0534fcd79de273742a532c0068f
Saiful Islam <saiful.apm@gmail.com>
2025-03-06 12:32:26 +0600

    (refactor) refactor code to fix some issues

--- lua/commasemi/utils.lua
+++ lua/commasemi/utils.lua
@@ -1,66 +1,78 @@
 local M = {}
 
--- Function to handle line toggle logic
+-- Improved function to toggle character at end of line with better Markdown support
 function M.toggle_single_line(line, character)
   if not line then return '' end
 
-  -- Find the earliest comment position
-  local content, comment
+  -- Simple pattern to split line at comment
+  local content, comment = line, ''
+
+  -- Common comment patterns
   local comment_patterns = {
-    { pattern = '%s+//' }, -- JavaScript, PHP style
-    { pattern = '%s+#' }, -- Python, Ruby style
-    { pattern = '%s+%-%-' }, -- Lua style
-    { pattern = '%s+/%*' }, -- C-style block
-    { pattern = '%s+<!%-%-' }, -- HTML style
-    { pattern = '%s+;' }, -- Assembly style
-    { pattern = '%s+%%' }, -- LaTeX style
-    { pattern = '%s+"""' }, -- Python docstring
-    { pattern = '%s+%%%' }, -- Erlang style
-    { pattern = '%s+%*' }, -- Doc comments
-    { pattern = '%s+%<%!' }, -- JSP style
-    { pattern = '%s+%<%%-' }, -- ERB style
-    { pattern = '%s+///' }, -- Rust doc comments
-    { pattern = '%s+%[%[' }, -- Lua block
+    '//[^"\']*$', -- C/C++/Java/JS style
+    '#[^"\']*$', -- Python/Ruby/Shell style
+    '%-%-[^"\']*$', -- Lua style
+    '/%*.*%*/$', -- C-style block on single line
+    '<!%-%-.*%-%->', -- HTML style
   }
 
-  -- Find comment position
-  local comment_pos
+  -- Find comment if present
   for _, pattern in ipairs(comment_patterns) do
-    local pos = line:find(pattern.pattern)
-    if pos and (not comment_pos or pos < comment_pos) then comment_pos = pos end
+    local s, e = line:find(pattern)
+    if s then
+      content = line:sub(1, s - 1)
+      comment = line:sub(s)
+      break
+    end
   end
 
-  -- Split line into content and comment
-  if comment_pos then
-    content = line:sub(1, comment_pos - 1)
-    comment = line:sub(comment_pos)
-  else
-    content = line
-    comment = ''
-  end
-
-  -- Trim trailing spaces from content
+  -- Strip trailing whitespace
   content = content:gsub('%s*$', '')
 
-  -- Toggle character
-  local last_char = content:sub(-1)
-  local new_content
+  -- Check for quotes to avoid modifying inside strings
+  local in_string = false
+  local skip_modification = false
+  local quote_chars = { '"', "'", '`' }
 
-  if last_char == ',' or last_char == ';' then
-    if last_char == character then
-      new_content = content:sub(1, -2)
-    else
-      new_content = content:sub(1, -2) .. character
+  -- Simple string detection (won't be perfect but better than nothing)
+  for i = 1, #content do
+    local char = content:sub(i, i)
+    if vim.tbl_contains(quote_chars, char) and (i == 1 or content:sub(i - 1, i - 1) ~= '\\') then
+      in_string = not in_string
     end
+
+    -- If we're at the end and still in a string, don't modify
+    if i == #content and in_string then skip_modification = true end
+  end
+
+  -- Skip modification in specific conditions
+  if skip_modification then return line end
+
+  -- Get filetype
+  local filetype = vim.bo.filetype or ''
+  local text_filetypes = { 'markdown', 'text', 'rst', 'org', 'txt', 'asciidoc' }
+  local is_text_file = vim.tbl_contains(text_filetypes, filetype)
+
+  -- Basic toggle logic that applies to all file types
+  local last_char = content:sub(-1)
+
+  -- Handling toggle behavior
+  if last_char == character then
+    -- Remove the character if it's already there
+    content = content:sub(1, -2)
+  elseif last_char == ',' or last_char == ';' then
+    -- Replace with the new character if there's a different punctuation
+    content = content:sub(1, -2) .. character
   else
-    new_content = content .. character
+    -- User can toggle it off again if they don't want it
+    content = content .. character
   end
 
   -- Reconstruct the line
   if comment ~= '' then
-    return new_content .. comment
+    return content .. ' ' .. comment
   else
-    return new_content
+    return content
   end
 end
 
